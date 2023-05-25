7a996de58f51d42303e1c8ee3cacd0c2fd115136
yoffrey <76462775+yoffrey@users.noreply.github.com>
2023-05-25 13:23:54 -0700

    changes

--- craigslist/src/pages/index.js
+++ craigslist/src/pages/index.js
@@ -8,8 +8,7 @@ function Home(){
     const [showBrowseButton, setShowBrowseButton] = useState(true);
     const [showUpload, setShowUpload] = useState(false);
     const [showUploadButton, setShowUploadButton] = useState(true);
-    const [showMap, setShowMap] = useState(false);
-    const [showMapButton, setShowMapButton] = useState(true);
+
     const onClickBrowse = () => {
         setShowBrowse(true);
         setShowBrowseButton(false);
@@ -20,28 +19,16 @@ function Home(){
         setShowUpload(true);
         setShowBrowseButton(false);
         setShowUploadButton(false);
-        setShowMapButton(false);
-    }
-
-    const onClickMap = () => {
-        setShowMap(true);
-        setShowBrowseButton(false);
-        setShowUploadButton(false);
-        setShowMapButton(false);
     }
 
     return (
         <>
             <div className={'home-button-container'}>
                 {showBrowseButton ? <button className={'browse-button'} onClick={onClickBrowse}>Browse</button>: null}
-                {showMapButton ? <button className={'browse-button'} onClick={onClickMap}>Map</button>: null}
                 {showUploadButton ? <button className={'browse-button'} onClick={onClickUpload}>Upload</button>: null}
             </div>
             <div className={`hidden-browse-container ${showBrowse ? 'visible-browse' : ''}`}>
                 {<MainCategory />}
-            </div>
-            <div className={`hidden-map-container ${showMap ? 'visible-map' : ''}`}>
-                {<MainMap />}
             </div>
         </>
     );
