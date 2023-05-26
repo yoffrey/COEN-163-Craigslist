962b4344bd3525e9f2a796e540aae13dc158afea
adw1029 <andrewcui1029@gmail.com>
2023-05-26 16:22:29 -0700

    Update MainMap.js

--- craigslist/src/components/MainMap.js
+++ craigslist/src/components/MainMap.js
@@ -1,13 +1,25 @@
 import React, {useState, useEffect} from "react";
 import './MainCategory.css';
 import SubMap from "./SubMap";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet" 
+import 'leaflet/dist/leaflet.css';
+
 
 function MainMap() {
+    const position = [37.3494, -121.9381];
 
-    return (
-        <>
-            <div>hi test main map</div>
-        </>
+    return ( 
+        <MapContainer center={position} zoom={13} scrollWheelZoom={false} style = {{height: '100%', width: '100%', position: "absolute"}}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     );
 
 }
