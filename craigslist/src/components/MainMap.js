b0665fe425c562fb85fe776e2e1027f38dc496c4
adw1029 <andrewcui1029@gmail.com>
2023-05-27 00:28:22 -0700

    Update MainMap.js

--- craigslist/src/components/MainMap.js
+++ craigslist/src/components/MainMap.js
@@ -1,12 +1,18 @@
 import React, {useState, useEffect} from "react";
 import './MainCategory.css';
 import SubMap from "./SubMap";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet" 
+import { MapContainer, TileLayer, Marker, Popup, Tooltip } from "react-leaflet" 
 import 'leaflet/dist/leaflet.css';
+import {Icon} from "leaflet"
 
 
 function MainMap() {
     const position = [37.3494, -121.9381];
+
+    const customIcon = new Icon({
+      iconUrl: require("../images/placeholder.png"),
+      iconSize: [38, 38]
+    });
 
     return ( 
         <MapContainer center={position} zoom={13} scrollWheelZoom={false} style = {{height: '100%', width: '100%', position: "absolute"}}>
@@ -14,11 +20,24 @@ function MainMap() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
+        <Marker position={position} icon = {customIcon}> 
+          <Tooltip>
+            Item 1
+          </Tooltip>
         </Marker>
+
+        <Marker position={[37.3464, -121.9399]} icon = {customIcon}> 
+          <Tooltip>
+            Item 2
+          </Tooltip>
+        </Marker>
+
+        <Marker position={[37.3454, -121.9351]} icon = {customIcon}> 
+          <Tooltip>
+            Item 3
+          </Tooltip>
+        </Marker>
+        
       </MapContainer>
     );
 
