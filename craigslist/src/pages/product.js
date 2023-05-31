0e08dc3135b283cf70f19919c0cd78f5692e2d05
yoffrey <76462775+yoffrey@users.noreply.github.com>
2023-05-30 19:05:00 -0700

    Fix image scroll

--- craigslist/src/pages/product.js
+++ craigslist/src/pages/product.js
@@ -12,8 +12,11 @@ const Product = ()  => {
         if (imageChoice === bike) {
             setImageChoice(back);
         }
-        else {
-            setImageChoice(front)
+        if (imageChoice === front) {
+            setImageChoice(bike);
+        }
+        if (imageChoice === back) {
+            setImageChoice(front);
         }
     }
 
@@ -21,8 +24,11 @@ const Product = ()  => {
         if (imageChoice === bike) {
             setImageChoice(front);
         }
-        else {
-            setImageChoice(back)
+        if (imageChoice === front) {
+            setImageChoice(back);
+        }
+        if (imageChoice === back) {
+            setImageChoice(bike);
         }
     }
 
@@ -32,6 +38,9 @@ const Product = ()  => {
                 <img src={imageChoice} alt="Product Image" />
                     <button id="prev-button" className="nav-button" onClick={handlePrevious}>Previous</button>
                     <button id="next-button" className="nav-button" onClick={handleNext}>Next</button>
+            </div>
+            <div>
+                DESCRIPTION
             </div>
         </div>
     );
