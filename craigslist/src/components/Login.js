23450b7038fa973c882ae80bd0a760c783ba90e3
adw1029 <andrewcui1029@gmail.com>
2023-05-30 19:13:46 -0700

    login in update

--- craigslist/src/components/Login.js
+++ craigslist/src/components/Login.js
@@ -1,9 +1,30 @@
 import React, {useState} from "react";
+import "./Login.css"
 
-function Login() {
+const Login = ({isShowLogin}) => {
     return (
-        <div className={'login-container'}>
-            hi
+        <div className={'${!isShowLogin ? "active": ""} show'}>
+            <div className="login-form">
+                <div className="form-box solid">
+                    <form>
+                        <h1 className="login-text"> Sign In</h1>
+                        <label>Username</label><br></br>
+                        <input 
+                            type="text"
+                            name="username"
+                            className="login-box"/> <br></br>
+
+                        <label>Password</label><br></br>
+                        <input 
+                            type="password"
+                            name="password"
+                            className="login-box"/> <br></br>
+                        <input type="submit" value="LOGIN" className="login-btn"/>
+                        <input type="submit" value="REGISTER" className="register-btn"/>
+                    </form>
+                </div>
+            </div>
+        
         </div>
     );
 }
