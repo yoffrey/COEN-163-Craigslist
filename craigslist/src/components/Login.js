acc2847574bd60af93641c6eb3d018464021a00d
adw1029 <andrewcui1029@gmail.com>
2023-06-01 12:50:26 -0700

    login update

--- craigslist/src/components/Login.js
+++ craigslist/src/components/Login.js
@@ -1,26 +1,30 @@
 import React, {useState} from "react";
 import "./Login.css"
+import { Link } from "react-router-dom";
+
 
 const Login = ({isShowLogin}) => {
     return (
         <div className={'${!isShowLogin ? "active": ""} show'}>
             <div className="login-form">
-                <div className="form-box solid">
+                <div className="form-box-solid">
                     <form>
                         <h1 className="login-text"> Sign In</h1>
-                        <label>Username</label><br></br>
+                        <label className="usr-name">Username</label><br></br>
                         <input 
                             type="text"
                             name="username"
                             className="login-box"/> <br></br>
 
-                        <label>Password</label><br></br>
+                        <label className = "usr-name">Password</label><br></br>
                         <input 
                             type="password"
                             name="password"
-                            className="login-box"/> <br></br>
+                            className="login-box"/> <br></br> <br></br>
                         <input type="submit" value="LOGIN" className="login-btn"/>
+                        <Link to={'/register'} >
                         <input type="submit" value="REGISTER" className="register-btn"/>
+                        </Link>
                     </form>
                 </div>
             </div>
