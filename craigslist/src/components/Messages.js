7b3d6d39b2eed0b858f4dee3bd322f2c8fd7ebbb
yoffrey <76462775+yoffrey@users.noreply.github.com>
2023-06-05 00:19:41 -0700

    Fix some message bugs

--- craigslist/src/components/Messages.js
+++ craigslist/src/components/Messages.js
@@ -1,15 +1,15 @@
 import React, {useState} from 'react';
-import { Link } from "react-router-dom";
 import './Messages.css'
 
 function Messages() {
+    const [messageInput, setMessageInput] = useState("Enter message...");
     const unhide = () => {
-      var name = document.getElementById("welcomeForm").value;
+      setMessageInput("Enter message...");
+      const name = document.getElementById("welcomeForm").value;
       document.getElementById("hidden").style.display = "block";
       document.getElementById("nextmsg").innerHTML = "You: " + name;
-      var today = new Date();
-      var time = today.getHours() + ":" + today.getMinutes();
-      document.getElementById("time-now").innerHTML = time;
+      const today = new Date();
+      document.getElementById("time-now").innerHTML = today.getHours() + ":" + today.getMinutes();
     }
     const openForm = () => {
         document.getElementById("myForm0").style.display = "block";
@@ -25,72 +25,89 @@ function Messages() {
       const closeForm1 = () => {
         document.getElementById("myForm0").style.display = "none";
       }
+
+      const handleMessageChange = (e) => {
+        e.preventDefault();
+        setMessageInput(e.value);
+      }
+
+  const handleInputClick = () => {
+      setMessageInput('');
+  }
+
+  const handleMessageEnter = (e) => {
+      if (e.key === "Enter"){
+        e.preventDefault();
+        unhide();
+      }
+
+  }
+
   return (
     <>
-<div class="chat-popup" id="myForm0">
-  <form action="/action_page.php" class="form-container">
-    <h1>All Messages</h1>
-    <div class="message">
+<div className="chat-popup" id="myForm0">
+  <form className="form-container">
+    <div className={'message-header'}>Messages</div>
+    <div className="message">
 
-    <div class="all">
-  <p>The Cat</p>
-  <button type="button" class="btn message" onClick={openForm1}>Message</button>
-</div>
-
-<div class="all">
-  <p>Sunglasses</p>
-  <button type="button" class="btn message" onClick={openForm1}>Message</button>
+    <div className="all">
+  <div className={'chat-title'}>The Cat</div>
+  <button type="button" className="btn message" onClick={openForm1}>Message</button>
 </div>
 
-<div class="all">
-  <p>Panigale V4</p>
-    <button type="button" class="btn message" onClick={openForm1}>Message</button>
+<div className="all">
+  <div className={'chat-title'}>Sunglasses</div>
+  <button type="button" className="btn message" onClick={openForm1}>Message</button>
 </div>
 
-<div class="all">
-  <p>Mona Lisa 2021</p>
-    <button type="button" class="btn message" onClick={openForm1}>Message</button>
+<div className="all">
+  <div className={'chat-title'}>Panigale V4</div>
+    <button type="button" className="btn message" onClick={openForm1}>Message</button>
+</div>
+
+<div className="all">
+  <div className={'chat-title'}>Mona Lisa 2021</div>
+    <button type="button" className="btn message" onClick={openForm1}>Message</button>
 </div>
 </div>
-    <button type="button" class="btn cancel" onClick={closeForm1}>Close</button>
+    <button type="button" className="btn cancel" onClick={closeForm1}>Close</button>
   </form>
 </div>
 
-<div class="chat-popups" id="myForm">
-  <form action="/action_page.php" class="form-container">
+<div className="chat-popups" id="myForm">
+  <form className="form-container">
     <h1>Chat</h1>
 
-    <label for="msg"><b>The Cat</b></label>
-    <div class="message">
-    <div class="container">
+    <label form="msg"><b>The Cat</b></label>
+    <div className="message">
+    <div className="container">
   <p>You: Hello. How are you today?</p>
-  <span class="time-right">11:00</span>
+  <span className="time-right">11:00</span>
 </div>
 <hr></hr>
-<div class="container darker">
+<div className="container darker">
   <p>Other: Hey! I'm fine. Thanks for asking!</p>
-  <span class="time-left">11:01</span>
+  <span className="time-left">11:01</span>
 </div>
 <hr></hr>
-<div class="container">
+<div className="container">
   <p>You: Alright, where do you live so we can decide where to meet up?</p>
-  <span class="time-right">11:02</span>
+  <span className="time-right">11:02</span>
 </div>
 <hr></hr>
-<div class="container darker">
+<div className="container darker">
   <p>Other: I live in Santa Clara</p>
-  <span class="time-left">11:05</span>
+  <span className="time-left">11:05</span>
 </div>
 <hr></hr>
-<div class="container" id="hidden">
+<div className="container" id="hidden">
   <p id="nextmsg">You:Great!</p>
-  <span id="time-now" class="time-right">11:10</span>
+  <span id="time-now" className="time-right">11:10</span>
 </div>
-<input type="text" placeholder="Type message.." id="welcomeForm" name="msg" required></input>
 </div>
-
-    <button type="button" class="btn" onClick={unhide}>Send</button>
-    <button type="button" class="btn cancel" onClick={closeForm}>Close</button>
+    <input className={'chat-input'} type="text" value={messageInput} id="welcomeForm" name="msg" onChange={handleMessageChange} onClick={handleInputClick} onKeyDown={handleMessageEnter} required></input>
+    <button type="button" className="btn" onClick={unhide}>Send</button>
+    <button type="button" className="btn cancel" onClick={closeForm}>Close</button>
   </form>
 </div>
     </>
